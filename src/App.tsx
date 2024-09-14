1f2c5d232b0e6f4b6297d149cbe258e914e06442
OnuegbulamChristian <christianonuegbulam@gmail.com>
2024-09-14 05:06:43 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ import Productlist from "./Components/Product/Productlist";
 import UserScreen from "./Screens/UserScreen/UserScreen";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserProfile from "./Screens/UserScreen/UserProfile";
+import SignUp from "./Components/SignUpScreen/SignUpScreen";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<UserScreen />} />
-          <Route path="/profile/:id" element={<UserProfile />} />
+          {/* <Route path="/" element={<UserScreen />} />
+          <Route path="/profile/:id" element={<UserProfile />} /> */}
+          <Route path="/" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
       {/* <Product /> */}
